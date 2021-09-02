fcf4e46ea682611922fe8d589868e903243ccfae
kksumm123 <kksumm123@gmail.com>
2021-09-03 03:31:34 +0900

    Bullet.cs 추가

---
+++ New Unity Project/Assets/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 15f;
+    [SerializeField] float destroyTime = 7f;
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * transform.forward);
+    }
+}
